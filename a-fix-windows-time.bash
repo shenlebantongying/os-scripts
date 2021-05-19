58b60f32497aeb78b33fe3d649dd412a47c74c4c
shenleban tongying <shenlebantongying@gmail.com>
2021-05-19 11:52:03 -0400

    setup-texlive.bash

--- a-fix-windows-time.bash
+++ a-fix-windows-time.bash
@@ -1,3 +1,10 @@
 #!/bin/sh
 timedatectl set-local-rtc 1 --adjust-system-clock
 timedatectl
+
+#Warning: The system is configured to read the RTC time in the local time zone.
+#This mode cannot be fully supported. It will create various problems
+#with time zone changes and daylight saving time adjustments. The RTC
+#time is never updated, it relies on external facilities to maintain it.
+#If at all possible, use RTC in UTC by calling
+#'timedatectl set-local-rtc 0'.
---
+++ setup-texlive.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT=$(realpath "$0") # -> Complete path /home/slbtty/scripts/sourcetest.bash
+SCRIPTPATH=$(dirname "$SCRIPT") #-> only /home/slbtty/scripts
+
+cd ${SCRIPTPATH}
+
+curl -SL https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz -o install-tl-unx.tar.gz
+mkdir install_tl && tar xf install-tl-unx.tar.gz -C install_tl --strip-components 1
+
+cd ./install_tl
+sudo ./install-tl -init-from-profile ../texlive.profile
+
+cd ../
+rm ./install-tl-unx.tar.gz
+rm -rf ./install_tl
