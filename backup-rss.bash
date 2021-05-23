1bbb7bbe376166a1a402a11605e9bd1570c4f06e
shenleban tongying <shenlebantongying@gmail.com>
2021-05-23 06:12:03 -0400

    Migarte form akregator to newboat

--- backup-rss.bash
+++ backup-rss.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPT=$(realpath "$0") # -> Complete path /home/slbtty/scripts/sourcetest.bash
 SCRIPTPATH=$(dirname "$SCRIPT") #-> only /home/slbtty/scripts
 
-cp ~/.local/share/akregator/data/feeds.opml "$SCRIPTPATH"/rss.opml
+# Old Akregator
+# cp ~/.local/share/akregator/data/feeds.opml "$SCRIPTPATH"/rss.opml
+
+cp ~/.newsboat/urls ${SCRIPTPATH}/rss.urls
+/usr/share/doc/newsboat/contrib/exportOPMLWithTags.py ~/.newsboat/urls > ${SCRIPTPATH}/rss.opml
+
+echo "OK"
