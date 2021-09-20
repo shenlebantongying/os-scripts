700be8b16be0193b41c1d62be232ed8cbb8b29f6
shenleban tongying <shenlebantongying@gmail.com>
2021-09-20 19:17:04 -0400

    fix mac fish handling

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,15 +1,31 @@
 if status is-interactive
-    set PATH \
-        $HOME/.ghcup/bin \
-        $HOME/.local/bin \
-        $HOME/bin \
-        $HOME/s \
-        $HOME/.dotnet \
-        $HOME/.cabal/bin \
-        $HOME/.rbenv/bin \
-        $HOME/.cargo/bin \
-        $HOME/.emacs.d/bin \
-        $HOME/.local/share/coursier/bin \
-        /usr/local/Wolfram/Mathematica/12.3/Executables \
-    $PATH
+    switch (uname)
+        case Linux
+            set PATH \
+                $HOME/.ghcup/bin \
+                $HOME/.local/bin \
+                $HOME/bin \
+                $HOME/s \
+                $HOME/.dotnet \
+                $HOME/.cabal/bin \
+                $HOME/.rbenv/bin \
+                $HOME/.cargo/bin \
+                $HOME/.emacs.d/bin \
+                $HOME/.local/share/coursier/bin \
+                /usr/local/Wolfram/Mathematica/12.3/Executables \
+            $PATH
+        case Darwin
+            set PATH \
+                $HOME/.ghcup/bin \
+                $HOME/.local/bin \
+                $HOME/bin \
+                $HOME/scripts \
+                $HOME/.cabal/bin \
+                $HOME/.rbenv/bin \
+                $HOME/.cargo/bin \
+                $HOME/.emacs.d/bin \
+            $PATH
+        case '*'
+                echo !!!! OS undetectable
+    end
 end
