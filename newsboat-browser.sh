bc2baa911c126f65a25b5671641d05b92be2db1d
shenleban tongying <shenlebantongying@gmail.com>
2021-10-06 22:17:34 -0400

    add macos support

--- newsboat-browser.sh
+++ newsboat-browser.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-if command -v firefox &> /dev/null
+if command -v xdg-open &> /dev/null
+then
+    xdg-open "$@" &
+elif command -v open &> /dev/null
+then
+    open "$@" &
+elif command -v firefox &> /dev/null
 then
     firefox "$@" &
 elif command -v firefox-developer-edition &> /dev/null
 then
     firefox-developer-edition "$@" &
 fi
+
