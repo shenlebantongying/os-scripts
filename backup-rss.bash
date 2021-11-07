101d917c13d953c9f98ed96dc08c54a1286b31e3
shenleban tongying <shenlebantongying@gmail.com>
2021-11-06 22:22:50 -0400

    make sure local & repo rss urls synced.

--- backup-rss.bash
+++ backup-rss.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 SCRIPT=$(realpath "$0") # -> Complete path /home/slbtty/scripts/sourcetest.bash
 SCRIPTPATH=$(dirname "$SCRIPT") #-> only /home/slbtty/scripts
@@ -12,14 +12,16 @@ SCRIPTPATH=$(dirname "$SCRIPT") #-> only /home/slbtty/scripts
 diff ~/.newsboat/urls "${SCRIPTPATH}"/rss.urls && echo "no new urls" || echo "=[New Urls]="
 
 if [[ -f ~/.newsboat/urls ]]; then
-    cp ~/.newsboat/urls "${SCRIPTPATH}"/rss.urls
+    sdiff ~/.newsboat/urls "${SCRIPTPATH}"/rss.urls -o ~/.newsboat/urls_temp
+    cp ~/.newsboat/urls_temp ~/.newsboat/urls 
+    cp ~/.newsboat/urls_temp  "${SCRIPTPATH}"/rss.urls
+    rm ~/.newsboat/urls_temp
 
     if [[ $(uname -m) == 'arm64' ]]; then
         /opt/homebrew/share/doc/newsboat/contrib/exportOPMLWithTags.py ~/.newsboat/urls > "${SCRIPTPATH}"/rss.opml
     else
         /usr/share/doc/newsboat/contrib/exportOPMLWithTags.py ~/.newsboat/urls > "${SCRIPTPATH}"/rss.opml
     fi
-    
 else 
     cp "${SCRIPTPATH}"/rss.urls ~/.newsboat/urls 
 fi
