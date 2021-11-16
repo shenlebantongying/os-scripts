f77d119d7bda3f0984fbbe4b17c9d8c174b53223
shenleban tongying <shenlebantongying@gmail.com>
2021-11-16 01:16:52 -0500

    unit for large file converter

--- a-large-file-finder.rkt
+++ a-large-file-finder.rkt
@@ -7,6 +7,14 @@
 ; TODO: add option to ignore hidden file?
 ; TDOO: stop using `fold-files` because it cannot do find file concurrently
 
+(define (filesize->readable nbytes)
+  (if (= nbytes 0)
+      "0 B"
+      (let* ([units '("B" "KiB" "MiB" "GiB" "TiB" "PiB" "EiB" "ZiB" "YiB")]
+             [i (exact-floor (log nbytes 1024))]
+             [s (exact->inexact (/ nbytes (expt 1024 i)))])
+        (format "~a ~a" (~r s #:precision 2) (list-ref units i)))))
+
 (define parent-dir
   (command-line
    #:args (pname)
@@ -14,7 +22,7 @@
 
 (for-each
  (λ (arg)
-   (printf "~a ~a\n" (second arg) (first arg)))
+   (printf "~a ~a\n" (filesize->readable (second arg)) (first arg)))
  (sort
   (fold-files
    (λ (fpath type acc)
