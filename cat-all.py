c1dcb0805402aef0149dc4905d9c366693188637
shenleban tongying <shenlebantongying@gmail.com>
2021-12-12 11:16:05 -0500

    robuster cat-all

--- cat-all.py
+++ cat-all.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
-import os
-import subprocess
+import os,sys, subprocess
 
 # this is intended to be used with as
 # cat-all.py | fzf
 
-IGNORE=[".git",".vscode"]
-
+IGNORE_DIRS=[".git",".vscode","build"]
 pwd=os.getcwd()
-cwdlength=len(pwd)
 
 for  (dirpath, dirnames, filenames) in os.walk(pwd):
-    if not any(ig in dirpath for ig in IGNORE):
+    if not any(ig in dirpath for ig in IGNORE_DIRS):
         for file in filenames:
-            fullpath=dirpath+"/"+file
-            ftype=subprocess.check_output(["file","--dereference","--brief", "--mime-type", fullpath]).decode()
+            full_path=dirpath+"/"+file
+            relative_path=full_path[len(pwd)+1:]
+
+            mimetype=subprocess.check_output(["file","--dereference","--brief", "--mime-type", full_path]).decode()
             
-            if(ftype.startswith("text")):
-                with open(fullpath,"r") as f:
-                    for ln,line in enumerate(f,start=1):
-                        if not line.isspace():
-                            print(fullpath[cwdlength+1:],"|",ln,"|",line.strip())
+            if(mimetype.startswith("text")):
+                try:
+                    with open(full_path,"r") as f:
+                        for ln,line in enumerate(f,start=1):
+                            if not line.isspace():
+                                print(relative_path,"|",ln,"|",line.strip())
+                except:
+                    print("[FAILED] ",full_path, file=sys.stderr) 
