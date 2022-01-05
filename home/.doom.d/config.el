7542f828d1170c38eb7d50f3a1e9f29e42185d0e
shenleban tongying <shenlebantongying@gmail.com>
2022-01-05 15:01:18 -0500

    ok

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -5,6 +5,8 @@
 ;; sync' after modifying this file!
 
 (setq default-tab-width 4)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60))
 
 (menu-bar-mode)
