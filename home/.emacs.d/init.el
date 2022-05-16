b3039bfe81038ade4003693d4715ad70e61378c9
shenleban tongying <shenlebantongying@gmail.com>
2022-05-16 08:32:07 -0400

    privatize all doom emacs!

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,180 @@
+;;; -*- lexical-binding: t; -*-
+(defconst IS-MAC     (eq system-type 'darwin))
+(defconst IS-LINUX   (eq system-type 'gnu/linux))
+
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+
+
+(when IS-MAC
+  (dolist (dir '("/Applications/Racket v8.5/bin/racket"))
+    (add-to-list 'exec-path dir)))
+
+(global-unset-key (kbd "C-z"))
+(global-unset-key (kbd "C-<wheel-down>"))
+(global-unset-key (kbd "C-<wheel-up>"))
+
+;; Setup straight and use-package
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(when IS-MAC
+  (use-package exec-path-from-shell
+    :straight t
+    :init
+    (exec-path-from-shell-initialize)))
+
+;; vertico, orderless, marginalia, 
+(use-package vertico
+  :straight t
+  :init
+  (vertico-mode)
+  (setq vertico-cycle t))
+
+
+(use-package orderless
+  :straight t
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Enable richer annotations using the Marginalia package
+(use-package marginalia
+  :straight t
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
+;; Example configuration for Consult
+(use-package consult
+  :straight t
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-recent-file
+   consult--source-project-recent-file
+   :preview-key (kbd "M-."))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;; There are multiple reasonable alternatives to chose from.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 3. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 4. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  )
+
+(use-package corfu
+  :straight t
+  :init
+  (setq corfu-auto t
+      corfu-quit-no-match 'separator)
+  (global-corfu-mode))
+
+(use-package modus-themes
+  :straight t
+  :ensure
+  :init
+)
+
+;; [ +lang ]
+
+(use-package racket-mode
+  :straight t
+  :mode "\\.rkt\\'"  ; give it precedence over :lang scheme
+  :config
+  (setq racket-xp-after-change-refresh-delay 3))
+
+(load-theme 'modus-operandi t)
+
+;; [ Personal config ]
+
+(tool-bar-mode -1)
+(menu-bar-mode)
+(display-line-numbers-mode)
+
+(setq initial-frame-alist '((width . 100) (height . 50)))
+(setq-default cursor-type 'bar)
+
+(recentf-mode 1)
+(setq recentf-max-saved-items 100)
+(global-set-key (kbd "s-r") 'consult-recent-file)
+
+(when IS-MAC
+  (set-face-attribute 'default nil :font "Ubuntu Mono" :height 150)
+  (setq mac-function-modifier 'hyper)
+)
+
+
+;; [ +bind.el ]
+
+(global-set-key (kbd "s-b") 'consult-buffer)
